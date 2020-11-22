fe94bbfe49a2377cacfc72f6cb825063ce01008e
sp2382 <70597438+sp2382@users.noreply.github.com>
2020-11-22 16:18:14 -0500

    Milestone 2

---
+++ Sockets/src/client/User.java
@@ -0,0 +1,27 @@
+package client;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class User extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private JTextField nameField;
+
+	public User(String name) {
+		this.name = name;
+		nameField = new JTextField(name);
+		nameField.setEditable(false);
+		this.setLayout(new BorderLayout());
+		this.add(nameField);
+	}
+
+	public String getName() {
+		return name;
+	}
+}
